010cbf70041911413e4f7364cb2bb32e81fbd6d4
Emiel de Jong <emieldejong@xs4all.nl>
2021-02-25 10:58:41 +0100

    wake up conrtoller comment

--- src/main/java/nl/saxofoonleren/dropit/controller/HomeController.java
+++ src/main/java/nl/saxofoonleren/dropit/controller/HomeController.java
@@ -3,11 +3,12 @@ package nl.saxofoonleren.dropit.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "https://dropit.netlify.app/", maxAge = 3600)
 @RestController
 @RequestMapping("api/home")
 public class HomeController {
 
+    // this end-point in only used to wake up the sleeping heroku server
     @GetMapping("/wake-up")
     public ResponseEntity<?>wakeUp() {
             return ResponseEntity.ok("waking up!");
